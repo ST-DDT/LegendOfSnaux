553e6eb872273bddba0fd03a6d01f0652efcba16
Christopher Quadflieg <chrissi92@hotmail.de>
2019-02-16 11:28:49 +0100

    initialize blocks and chunks

---
+++ Assets/ChunkGenerator.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChunkGenerator : MonoBehaviour
+{
+    Dictionary<Vector3Int, Chunk> chunks = new Dictionary<Vector3Int, Chunk>();
+
+    void Awake()
+    {
+        for (int x = 0; x < 3; x++)
+        {
+            for (int z = 0; z < 3; z++)
+            {
+                GameObject go = new GameObject();
+                Chunk chunk = go.AddComponent<Chunk>();
+                go.transform.parent = gameObject.transform;
+                go.transform.localPosition = new Vector3(x * 16, 0, z * 16);
+                chunk.Awake();
+
+                chunks.Add(new Vector3Int(x, 0, z), chunk);
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}

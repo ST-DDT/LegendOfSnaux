890c5713fc55fccfd61ed319d2c251ba1f98ea0a
Christopher Quadflieg <chrissi92@hotmail.de>
2019-02-20 17:35:33 +0100

    cleaned out-commented code

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -12,8 +12,6 @@ public class Player : MonoBehaviour
 
 	public bool grounded = false;
 	private bool performJump = false;
-	//public float fallMultiplier = 2.5f;
-	//public float lowJumpMultiplier = 2f;
 
 	private void Awake()
 	{
@@ -39,17 +37,6 @@ public class Player : MonoBehaviour
 			Debug.Log($"Set rigidbody.useGravity to {myRigidbody.useGravity}");
 			myRigidbody.velocity.Set(0, 0, 0);
 		}
-
-		//// Source: https://www.youtube.com/watch?v=7KiK0Aqtmzc
-		//// Unfortunately that does not work well :(
-		//if (myRigidbody.velocity.y < 0)
-		//{
-		//	myRigidbody.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1f) * Time.deltaTime;
-		//}
-		//else if (myRigidbody.velocity.y > 0 && !Input.GetButton("Jump"))
-		//{
-		//	myRigidbody.velocity += Vector3.up * Physics.gravity.y * (lowJumpMultiplier - 1f) * Time.deltaTime;
-		//}
 
 		if (Input.GetButtonDown("Jump") && grounded && !performJump)
 		{
